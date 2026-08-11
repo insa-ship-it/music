327c4eb898101c23545649268652b9d9a24f6d61
insa-ship-it <leemichaelinsa@gmail.com>
2026-08-11 15:13:40 -0500

    Add Stingray playlist URL to the list

--- combine_remote_playlists_with_epg.py
+++ combine_remote_playlists_with_epg.py
@@ -8,6 +8,7 @@ PLAYLISTS = [
     "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/dancetv.m3u8", 
     "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/iheart.m3u8", 
     "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/5tvpro-music.m3u8", 
+    "https://github.com/insa-ship-it/demon_m3/raw/refs/heads/main/playlists/m3u/stingray.m3u8", 
     # Add more playlists here in the format: "URL_TO_PLAYLIST"
 ]
 
